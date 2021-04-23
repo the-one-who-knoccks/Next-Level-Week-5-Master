5e09fdf0bf5dc52cc2074cc549658ea5bc4f27b7
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-04-23 01:56:47 -0300

    Refactore the code
    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ interface Episode {
   id: string;
   title: string;
   thumbnail: string,
-  description: string,
   members: string;
   duration: number;
   durationAsString: string,
@@ -130,9 +129,7 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
-
     };
   })
 
