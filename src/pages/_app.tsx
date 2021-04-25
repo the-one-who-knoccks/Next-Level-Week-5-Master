61c73bd1aad279b02d1fb177748b1aa12db01989
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-04-25 12:39:44 -0300

    Refactore the code
    _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,37 @@
+
+
 import '../styles/global.scss';
+
+import {PlayerContext} from '../contexts/PlayerContext';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] =useState(0);
+  const [isPlaying, setIsPlaying] =  useState(false);
+
+  function handlePlayEpisode(episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  };
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex: 0, handlePlayEpisode, togglePlay, isPlaying, setPlayingState}}>
     <div className={styles.wrapper}>
       <main>
       <Header />
@@ -14,6 +39,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
   )
 }
 
