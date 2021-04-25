baaa9061b5c9b5dd53d758e30eece6f592261ede
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-04-25 12:41:27 -0300

    Created the contexts
    Context Api

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from 'react';
+
+interface Episode {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+}
+
+interface PlayerContextData  {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;   
+  isPlaying: boolean;
+  handlePlayEpisode: (episode: Episode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
